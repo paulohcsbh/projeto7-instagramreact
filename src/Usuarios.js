3713f4597186991991370b6af359f523b7ef2020
Paulo Henrique Cardoso da Silva <paulohcsbh@hotmail.com>
2022-09-18 02:02:45 -0300

    Final commit

--- src/Usuarios.js
+++ src/Usuarios.js
@@ -2,19 +2,33 @@ import React from "react";
 export default function Usuarios(){  
   const [nome, setNome] = React.useState("");
   const [img, setImg] = React.useState("https://cdn-icons-png.flaticon.com/512/18/18601.png")
+  function Usuario(props){
+    return(
+    <li class="usuario">
+        <img onClick={props.click} src={props.src} alt={props.alt}/>
+        <div class="texto">
+          <strong>{props.strong}</strong> 
+          <div class="editar">
+          <p>{props.p}</p> <span class="lapis" onClick={props.click1}><ion-icon name="pencil"></ion-icon></span>
+        </div>
+        </div>
+        
+    </li>
+)
+  }
+
+
 
     return(
-  <div class="sidebar">
-  <div class="usuario">
-    <img onClick={() => setImg(prompt("Insira a URL da sua imagem"))} src={img} alt="Cadê sua imagem bonitona?" />
-    <div class="texto">
-      <strong>{(nome === "" || nome === null) ? "": nome.toLowerCase().replace(/ /g,"")}</strong>
-      <div class="editar">
-      <p>{nome}</p> <span class="lapis" onClick={() => setNome(prompt("Qual é o seu nome?"))}><ion-icon name="pencil"></ion-icon></span>
-      </div>
-    </div>
-  </div>
-  </div>
+  <ul class="sidebar">
+    <Usuario
+         click={() => setImg(prompt("Insira a URL da sua imagem"))} src={img} alt="Cadê sua imagem bonitona?"
+         
+         strong={(nome === "" || nome === null) ? "": nome.toLowerCase().replace(/ /g,"")}
+         p={nome} click1={() => setNome(prompt("Qual é o seu nome?"))}
+   />
+  
+  </ul>
   
 
     )
